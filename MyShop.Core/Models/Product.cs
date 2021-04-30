d77336a99d5148bca4055d04f69d2c1769f3d996
GustavoHRM <gustavohrm17@gmail.com>
2021-04-30 09:36:29 -0300

    Product Model and InMemory Product Repository Built

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
