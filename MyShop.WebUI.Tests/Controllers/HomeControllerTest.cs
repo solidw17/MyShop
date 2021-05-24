308c2c9afedca5f7f14c5c2d56b766d9fd3dabce
GustavoHRM <gustavohrm17@gmail.com>
2021-05-24 16:57:00 -0300

    Added Tests

--- MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 using System;
@@ -13,16 +16,19 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            // Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-            // Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
+            
+            HomeController controller = new HomeController(productContext, productCategoryContext);
 
-            // Assert
-            //Assert.IsNotNull(result);
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
 
     }
